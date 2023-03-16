59959c85aebabdb9c7623ac8c97933305d336e0d
emanuelerm <emanueler00@gmail.com>
2023-03-16 15:42:18 +0100

    even odd done

--- js/script.js
+++ js/script.js
@@ -48,8 +48,73 @@ function revealsPali() {
     } else {
         resultPali.innerText = `${paliInput.value} isn't a Palindrome`;
     }
-    
+
     paliInput.value = "";
 
 }
 
+
+// PARI E DISPARI
+
+let divEvenOdd = document.createElement("div")
+let selectEvenOdd = document.createElement("select");
+let options = ["pari", "dispari"];
+
+document.body.appendChild(divEvenOdd);
+divEvenOdd.appendChild(selectEvenOdd);
+
+for (let i = 0; i < options.length; i++) {
+    let optionsChoice = document.createElement("option");
+    optionsChoice.innerText = options[i];
+    selectEvenOdd.appendChild(optionsChoice);
+}
+
+let chooseNumberDiv = document.createElement("div");
+document.body.appendChild(chooseNumberDiv);
+
+let selectUserNumber = document.createElement("select");
+chooseNumberDiv.appendChild(selectUserNumber);
+let numberOptions = [1, 2, 3, 4, 5];
+
+for (let x = 0; x < numberOptions.length; x++) {
+    let numberChoice = document.createElement("option");
+    numberChoice.innerText = numberOptions[x];
+    selectUserNumber.appendChild(numberChoice);
+}
+
+const resultEvenOdd = document.createElement("p");
+const btnEvenOdd = document.createElement("button");
+
+btnEvenOdd.innerText = "Gioca";
+document.body.appendChild(resultEvenOdd);
+document.body.appendChild(btnEvenOdd);
+
+btnEvenOdd.addEventListener("click", playGame);
+
+
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+function isEven(n) {
+    return n % 2 === 0;
+}
+
+
+function playGame() {
+    let userChoice = selectEvenOdd.value;
+    let userNumber = selectUserNumber.value;
+
+    let computerNumber = getRandomInt(1, 6);
+
+    let sum = userNumber + computerNumber;
+
+    if (isEven(sum) && userChoice === "pari") {
+        resultEvenOdd.innerText = "Hai vinto!";
+    } else if (!isEven(sum) && userChoice === "dispari") {
+        resultEvenOdd.innerText = "Hai vinto!";
+    } else {
+        resultEvenOdd.innerText = "Hai perso!";
+    }
+}
